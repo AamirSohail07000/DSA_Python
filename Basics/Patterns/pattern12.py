a5ffa43a7cd691a25969d7e31d2f1b306ada6ec8
AamirSohail07000 <sohail.aamir07000@gmail.com>
2024-11-01 04:51:27 +0530

    updated pattern 12

--- Basics/Patterns/pattern12.py
+++ Basics/Patterns/pattern12.py
@@ -3,14 +3,12 @@
 # 123  321
 # 12344321
 
-n=5
-for i in range (1,n):
-  for j in range(1,i+1):
-    print(j,end="")
-  for j in range (i,n-1):
+n=4
+for i in range (n):
+  for j in range(i+1):
+    print(j+1,end="")
+  for j in range ((n-1-i)*2):
     print(" ",end="")
-  for j in range (i,n-1):
-    print(" ",end="")
-  for j in range(1,i+1):
+  for j in range(i+1):
     print(i-j+1,end="")
   print()      
