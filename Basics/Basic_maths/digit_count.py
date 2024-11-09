8204578fa860e4428e76b8180545d32a2d5ba62c
AamirSohail07000 <sohail.aamir07000@gmail.com>
2024-11-09 23:19:17 +0530

    "digit count"

--- Basics/Basic_maths/digit_count.py
+++ Basics/Basic_maths/digit_count.py
@@ -1,3 +1,17 @@
-N = int(input("Enter a number: "))
-count = len(str(abs(N))) # Use abs() to ignore the negative sign
-print(count)
+
+# def countNumber(num):
+#     return len(str(abs(num))) # Use abs() to ignore the negative sign
+
+
+# num = int(input("Enter a number : "))
+# result = countNumber(num)
+# print(result)
+
+# G4G Question
+# Given a number n. Count the number of digits in n which evenly divide n. Return an integer, total number of digits of n which divides n evenly.
+
+# Note :- Evenly divides means whether n is divisible by a digit i.e. leaves a remainder 0 when divided.
+
+def countNum(num):
+    return len(str(abs(num))) # Use abs() to ignore the negative sign
+  
