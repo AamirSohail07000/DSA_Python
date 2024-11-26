9e461395d5739e4b74e095552ee7ded1b466c77b
AamirSohail07000 <sohail.aamir07000@gmail.com>
2024-11-27 01:57:28 +0530

    Updated practice file

--- Basics/Basic_maths/practice.py
+++ Basics/Basic_maths/practice.py
@@ -10,36 +10,36 @@
 
 
 # OPTIMAL APPROACH
-import math
+# import math
 
-# Calculate the count of digits in 'n'
-# using logarithmic operation log10(n) + 1.
-def countDigits(n):
-    # Initialize a variable 'cnt' to
-    # store the count of digits.
-    cnt = int(math.log10(n) + 1)
+# # Calculate the count of digits in 'n'
+# # using logarithmic operation log10(n) + 1.
+# def countDigits(n):
+#     # Initialize a variable 'cnt' to
+#     # store the count of digits.
+#     cnt = int(math.log10(n) + 1)
 
-    # The expression int(math.log10(n) + 1)
-    # calculates the number of digits in 'n'
-    # and casts it to an integer.
+#     # The expression int(math.log10(n) + 1)
+#     # calculates the number of digits in 'n'
+#     # and casts it to an integer.
     
-    # Adding 1 to the result accounts
-    # for the case when 'n' is a power of 10,
-    # ensuring that the count is correct.
+#     # Adding 1 to the result accounts
+#     # for the case when 'n' is a power of 10,
+#     # ensuring that the count is correct.
    
-    # Finally, the result is cast
-    # to an integer to ensure it is rounded
-    # down to the nearest whole number.
+#     # Finally, the result is cast
+#     # to an integer to ensure it is rounded
+#     # down to the nearest whole number.
     
-    # Return the count of digits in 'n'.
-    return cnt
-# Main function
-if __name__ == "__main__":
-    # N = 329823
-    N = int(input("Enter a Number: "))
-    print("N:", N)
-    digits = countDigits(N)
-    print("Number of Digits in N:", digits)
+#     # Return the count of digits in 'n'.
+#     return cnt
+# # Main function
+# if __name__ == "__main__":
+#     # N = 329823
+#     N = int(input("Enter a Number: "))
+#     print("N:", N)
+#     digits = countDigits(N)
+#     print("Number of Digits in N:", digits)
 
 # G4G Question
 # Given a number n. Count the number of digits in n which evenly divide n. Return an integer, total number of digits of n which divides n evenly.
@@ -74,4 +74,33 @@ if __name__ == "__main__":
 # Constraints:
 
 # -231 <= x <= 231 - 1  
+
+# LCM GCD G4G
+# Given two integers a and b, Your task is to return a list containing their LCM and GCD.
+# Examples:
+# Input: a = 5 , b = 10
+# Output: [10, 5]
+# Explanation: LCM of 5 and 10 is 10, while their GCD is 5.
+
+import math
+
+def findLCMandGCD(a, b):
+    # Calculate the GCD using math.gcd
+    gcd = math.gcd(a, b)
+    
+    # Calculate the LCM using the formula: LCM = (a * b) / GCD
+    # We use integer division '//' to ensure the result is an integer
+    lcm = abs(a * b) // gcd
+
+    # Return the LCM and GCD as a list
+    return [lcm, gcd]
+
+# Example usage
+if __name__ == "__main__":
+    a = 12
+    b = 18
+    result = findLCMandGCD(a, b)
+    print(result)
+
+    
 
