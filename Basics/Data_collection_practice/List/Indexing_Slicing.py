1c220257e6c939311dcce4b1d320f47cf190f000
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-04-01 00:22:26 +0530

    rint the second element from the end

---
+++ Basics/Data_collection_practice/List/Indexing_Slicing.py
@@ -0,0 +1,11 @@
+		#  Given a list [10, 20, 30, 40, 50], print the second element from the end.
+
+
+list1 = [10, 20, 30, 40, 50]
+print(list1[(len(list1)-2)])
+
+# Using Negative indexing
+
+print(list1[-2])  # Output: 40
+
+# Note: Python allows negative indexing, where -1 refers to the last element, -2 to the second-last
