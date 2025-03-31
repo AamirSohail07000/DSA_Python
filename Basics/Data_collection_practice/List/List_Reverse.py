fd06d8a94434ef1c7127440f31760cd4709585c3
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-03-31 23:45:50 +0530

    Reverse a given list Using a Loop (Without reverse())

--- Basics/Data_collection_practice/List/List_Reverse.py
+++ Basics/Data_collection_practice/List/List_Reverse.py
@@ -1,5 +1,17 @@
 # Reverse a given list without using the reverse() function
 
+
+#  Using Slicing (Without reverse())
 list1 = [1, 2, 3, 4, 5]
 reversed_list = list1[::-1]  # Using slicing to reverse the list
+print(reversed_list)
+
+# Using a Loop (Without reverse())
+
+list1 = [1, 2, 3, 4, 5]
+reversed_list = []
+
+for i in range(len(list1) - 1, -1, -1):  # Loop from last index to 0
+    reversed_list.append(list1[i])
+
 print(reversed_list)
