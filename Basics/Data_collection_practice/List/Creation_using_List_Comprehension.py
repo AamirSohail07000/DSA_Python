947f9db6634dd21badca85d6158fa8105687cf68
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-04-01 00:35:00 +0530

    list containing the squares of numbers from 1 to 10 using list comprehension

---
+++ Basics/Data_collection_practice/List/Creation_using_List_Comprehension.py
@@ -0,0 +1,7 @@
+# Create a new list containing the squares of numbers from 1 to 10 using list comprehension.
+
+squares = [num ** 2 for num in range(1, 11)]
+print(squares)
+
+
+# The list comprehension [num ** 2 for num in range(1, 11)] stores the squared values in squares.
