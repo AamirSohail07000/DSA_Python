6af134e547cc5fb233097a04a82e3f9ff05b4057
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-03-31 23:37:31 +0530

    list of 5 numbers and print the sum of all elements

---
+++ Basics/Data_collection_practice/List/List_creation.py
@@ -0,0 +1,13 @@
+# Create a list of 5 numbers and print the sum of all elements.
+
+list1 = [3, 5, 6, 7, 23]
+
+
+# Printing index wise
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[4])
+
+print("Sum of elements in this list", sum(list1))
