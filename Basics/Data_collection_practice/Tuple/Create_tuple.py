6af9336d4a012786b6d1654d38dcf8f8080e13aa
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-04-03 23:22:11 +0530

    Create a tuple with at least 5 elements and print its length.

---
+++ Basics/Data_collection_practice/Tuple/Create_tuple.py
@@ -0,0 +1,7 @@
+# Create a tuple with at least 5 elements and print its length.
+
+# Creating a tuple with 5 elements
+my_tuple = (10, 20, 30, 40, 50)
+
+# Printing the length of the tuple
+print("Length of the tuple:", len(my_tuple))
