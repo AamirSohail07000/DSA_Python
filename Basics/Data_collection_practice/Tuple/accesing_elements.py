06ade28516aea0909d516b4e290ec6ab1a56e066
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-04-04 00:15:36 +0530

    print the third element

---
+++ Basics/Data_collection_practice/Tuple/accesing_elements.py
@@ -0,0 +1,10 @@
+# Given a tuple (1, 2, 3, 4, 5), print the third element.
+
+# Given tuple
+my_tuple = (1, 2, 3, 4, 5)
+
+# Accessing the third element (index 2)
+third_element = my_tuple[2]
+
+# Printing the third element
+print("Third element:", third_element)
