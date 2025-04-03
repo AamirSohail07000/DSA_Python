b09efb58cbc851b6bd0e418b1bd6c294fbd73ada
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-04-03 23:18:56 +0530

    Using count() Function

--- Basics/Data_collection_practice/List/element_existance_check.py
+++ Basics/Data_collection_practice/List/element_existance_check.py
@@ -16,3 +16,16 @@ if element_exists(numbers, target):
 else:
     print(f"{target} does not exist in the list.")
 
+# Method 2: Using count() Function
+
+def element_exists(lst, target):
+    return lst.count(target) > 0
+
+# Example Usage
+numbers = [1, 2, 3, 4, 5]
+target = 6
+
+if element_exists(numbers, target):
+    print(f"{target} exists in the list.")
+else:
+    print(f"{target} does not exist in the list.")
