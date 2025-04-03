7fb328179e7326c4024a7f7be2988ec367ac61a7
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-04-04 00:13:41 +0530

    Convert a tuple to a list and add a new element to it

---
+++ Basics/Data_collection_practice/Tuple/tuple_to_list_convert.py
@@ -0,0 +1,12 @@
+# Convert a tuple to a list and add a new element to it.
+# Creating a tuple
+my_tuple = (10, 20, 30, 40, 50)
+
+# Converting the tuple to a list
+my_list = list(my_tuple)
+
+# Adding a new element to the list
+my_list.append(60)
+
+# Printing the updated list
+print("Updated list:", my_list)
