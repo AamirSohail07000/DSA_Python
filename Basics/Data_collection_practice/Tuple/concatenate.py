b06099690654b190df14bc236768713e2dc1b08e
Aamir Sohail <155996028+AamirSohail07000@users.noreply.github.com>
2025-04-05 17:05:22 +0530

    Update concatenate.py

---
+++ Basics/Data_collection_practice/Tuple/concatenate.py
@@ -0,0 +1,11 @@
+# Given t1 = (1, 2, 3) and t2 = (4, 5, 6), concatenate them into a single tuple.
+
+# Given tuples
+t1 = (1, 2, 3)
+t2 = (4, 5, 6)
+
+# Concatenating tuples
+result = t1 + t2
+
+# Printing the result
+print("Concatenated Tuple:", result)
