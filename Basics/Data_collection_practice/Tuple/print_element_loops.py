29b8655d06bf2dfa66c2c5974993f9c2f5833bb3
Aamir Sohail <155996028+AamirSohail07000@users.noreply.github.com>
2025-04-05 17:03:57 +0530

    Update print_element_loops.py

---
+++ Basics/Data_collection_practice/Tuple/print_element_loops.py
@@ -0,0 +1,8 @@
+# Print each element of a tuple using a loop.
+
+# Given tuple
+my_tuple = (10, 20, 30, 40, 50)
+
+# Using a for loop to print each element
+for element in my_tuple:
+    print(element)
