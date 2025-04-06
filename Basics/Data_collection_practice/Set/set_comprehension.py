7db75ad925b9f3f52432b61bdfd732472ade8234
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-04-07 01:09:54 +0530

    Python program to generate a set of squares from 1 to 10 using set comprehension

---
+++ Basics/Data_collection_practice/Set/set_comprehension.py
@@ -0,0 +1,8 @@
+# Python program to generate a set of squares from 1 to 10 using set comprehension:
+
+
+# Generate set of squares using set comprehension
+squares = {x**2 for x in range(1, 11)}
+
+# Display the result
+print("Set of squares from 1 to 10:", squares)
