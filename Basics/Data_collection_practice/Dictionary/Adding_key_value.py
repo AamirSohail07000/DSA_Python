1fc6daaf8f2e0771090387441ef745d0b2e9a56b
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-04-10 19:37:06 +0530

    ython program to Add a new key-value pair to an existing dictionary

---
+++ Basics/Data_collection_practice/Dictionary/Adding_key_value.py
@@ -0,0 +1,20 @@
+# Write a python program to Add a new key-value pair to an existing dictionary. 
+
+# Existing dictionary of student scores
+student_scores = {
+    "Alice": 85,
+    "Bob": 92,
+    "Charlie": 78
+}
+
+# Adding a new student and their score
+student_scores["David"] = 88
+
+# Print the updated dictionary
+print("Updated student scores:", student_scores)
+
+# Adding a more student and their score
+student_scores["Mathew"] = 82
+
+# Print the updated dictionary
+print("Updated student scores:", student_scores)
