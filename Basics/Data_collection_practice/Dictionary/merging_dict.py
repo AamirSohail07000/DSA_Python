7f034937aec19fede5faae1af9ecf713441e291c
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-04-11 23:22:14 +0530

    merging dict using dict unpacking

--- Basics/Data_collection_practice/Dictionary/merging_dict.py
+++ Basics/Data_collection_practice/Dictionary/merging_dict.py
@@ -12,3 +12,14 @@ merged_dict = dict1.copy()
 merged_dict.update(dict2)
 
 print("Merged Dictionary:", merged_dict)
+
+
+# Method 2: Using dictionary unpacking
+
+dict1 = { "a": 1, "b": 2 }
+dict2 = { "c": 3, "d": 4 }
+
+# Merge using unpacking
+merged_dict = { **dict1, **dict2 }
+
+print("Merged Dictionary:", merged_dict)
