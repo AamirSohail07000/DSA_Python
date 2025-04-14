3b8960611de17086171b6b190d73476ec2650847
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-04-14 23:39:41 +0530

    dictionary of characters and their ASCII values using a string "hello"

--- Basics/Data_collection_practice/Dictionary/Dictionary_comprehension/ASCII_value_hello.py
+++ Basics/Data_collection_practice/Dictionary/Dictionary_comprehension/ASCII_value_hello.py
@@ -5,3 +5,24 @@ text = "hello"
 ascii_dict = {char: ord(char) for char in text}
 
 print(ascii_dict)
+
+
+# ✅ ord(char)
+# The ord() function in Python returns the ASCII (or Unicode) code of the given character.
+
+# For example:
+
+# ord('h') → 104
+
+# ord('e') → 101
+
+# ord('l') → 108
+
+# ord('o') → 111
+
+# ✅ {char: ord(char) for char in text}
+# This creates key-value pairs where:
+
+# key → character (like 'h')
+
+# value → its ASCII code (like 104)
